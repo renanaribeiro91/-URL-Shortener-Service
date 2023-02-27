a1c7ab6d3ea2f3641f29fdf89f8c13db20c48dba
renanaribeiro91 <renan.admribeiro@gmail.com>
2023-02-27 20:25:51 -0300

    feat: ajuste nas rotas

--- api/modules/url/route.js
+++ api/modules/url/route.js
@@ -1,14 +1,11 @@
 const { ControllerFactory } = require('simple-node-framework');
 const { route } = require('simple-node-framework').Singleton;
 const server = require('../../../index');
-const Controller = require('./controller');
+const UrlController = require('./controller');
 const validateURL = require('./middleware/validateUrl');
 
 
-// retreive route information
-// ex: { baseRoute: '/api', module: 'customer', full: '/api/customer' }
 const { full } = route.info(__filename);
 
-server.get(`${full}/:hash`, ControllerFactory.build(Controller, 'getUrl'));
-
-server.post(full, validateURL, ControllerFactory.build(Controller, 'startUrl'));
+server.get(`${full}/:hash`, ControllerFactory.build(UrlController, 'get'));
+server.post(`${full}/generate`, validateURL, ControllerFactory.build(UrlController, 'generate'));
