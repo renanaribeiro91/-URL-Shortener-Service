cbdf5ce72af2c726fbf7d55b72242261a77dd8e3
renanaribeiro91 <renan.admribeiro@gmail.com>
2023-02-27 20:25:34 -0300

    feat: add update and delete url controller

--- api/modules/url/controller.js
+++ api/modules/url/controller.js
@@ -1,48 +1,27 @@
 const { BaseController } = require('simple-node-framework').Base;
-const CustomerService = require('./service/url-service');
+const UrlService = require('./service/url-service.js');
 
 // sample controller
-class Controller extends BaseController {
+class UrlController extends BaseController {
     constructor() {
-        super({
-            module: 'My Sample Controller'
-        });
-        this.service = new CustomerService();
+        super({module: UrlController.name});
+        this.urlService = new UrlService();
     }
 
-    async startUrl(req, res, next) {
-        super.activateRequestLog(req);
+    
 
-        const { fullUrl } = req.body;
-
-        try {
-            this.log.debug(`Loading customer [${fullUrl}]`);
-
-            const customer = await this.service.start(fullUrl);
-
-            res.send(200, customer);
-
-
-            return next();
-        } catch (error) {
-            this.log.error('Unexpected error on load', error);
-            res.send(500, 'Unexpected error');
-            return next();
-        }
-    }
-
-    async getUrl(req, res, next) {
+    async get(req, res, next) {
         super.activateRequestLog(req);
 
         const { hash } = req.params;
 
         try {
-            this.log.debug(`Buscando original URl [${hash}]`);
+            this.log.debug(`Iniciando a busca da original URl com a hash :[${hash}]`);
 
-            const url = await this.service.getUrl(hash);
+            const url = await this.urlService.getUrl(hash);
 
             if (url) return res.send(200, url);
-            res.send(404, 'Cannot find URL');
+            res.send(404, { code: 'NOT_FOUND', message: 'Cannot find URL this hash' });
 
             return next();
         } catch (error) {
@@ -51,6 +30,72 @@ class Controller extends BaseController {
             return next();
         }
     }
+
+    async generate(req, res, next) {
+      super.activateRequestLog(req);
+
+      const { fullUrl } = req.body;
+
+      try {
+          const url = await this.urlService.createShortUrl(fullUrl);
+
+          if (url) return res.send(200, url); 
+          res.send(400, { message: 'URL already exists' })          
+
+          return next();
+      } catch (error) {
+          this.log.error('Unexpected error on load', error);
+          res.send(500, 'Unexpected error');
+          return next();
+      }
+  }
+
+    async update(req, res, next) {
+        super.activateRequestLog(req)
+    
+        const { hash } = req.params;
+        const { fullUrl } = req.body;
+    
+        try {
+        
+          const url = await this.service.update(hash, fullUrl)
+    
+          if (url) {
+            res.send(200, url)
+          } else {
+            res.send(404, { code: 'NOT_FOUND', message: 'Cannot find URL this hash' });
+          }
+    
+          return next()
+        } catch (error) {
+          this.log.error('Erro inesperado ao Atualizar', error)
+          res.send(500, { message: 'Unexpected error' })
+          return next()
+        }
+      }
+    
+      async delete(req, res, next) {
+        super.activateRequestLog(req)
+    
+        const { hash } = req.params;
+    
+        try {
+    
+          const url = await this.service.delete(hash)
+    
+          if (url) {
+            res.send(202)
+          } else {
+            res.send(404, { code: 'NOT_FOUND', message: 'Cannot find URL this hash' });
+          }
+    
+          return next()
+        } catch (error) {
+          this.log.error('Erro inesperado ao Deletar', error)
+          res.send(500, { message: 'Unexpected error' })
+          return next()
+        }
+      }
 }
 
-module.exports = Controller;
+module.exports = UrlController;
