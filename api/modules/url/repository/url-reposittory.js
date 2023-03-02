0ebd1f99d0c9d18227c6afb699b2d2ea6c9e9c1a
renanaribeiro91 <renan.admribeiro@gmail.com>
2023-03-02 17:22:39 -0300

    feat: ajuste update and delete

--- api/modules/url/repository/url-reposittory.js
+++ api/modules/url/repository/url-reposittory.js
@@ -1,35 +1,40 @@
 const { BaseRepository } = require('simple-node-framework').Base
-const { UrlModel } = require('../model/url')
+const UrlModel = require('../model/url')
 
 class UrlRepository extends BaseRepository {
   constructor() {
     super({ module: UrlRepository.name })
-    this.UrlModel = new UrlModel().db
+    this.urlModel = UrlModel
   }
 
   async getHash(hash) {
-    this.log.debug(`Buscando a hash: [${hash}]`)
-    return this.UrlModel.findOne({ hash })
+    // this.log.debug(`Buscando a hash: [${hash}]`)
+    return this.urlModel.findOne({ hash })
   }
 
   async getUrl(fullURL) {
-    this.log.debug(`Buscando a url: [${fullURL}]`)
-    return this.UrlModel.findOne({ fullURL })
+    // this.log.debug(`Buscando a url: [${fullURL}]`)
+    return this.urlModel.findOne({ fullURL })
   }
 
   async create(fullURL, hash, shortURL) {
-    this.log.debug(`Criando a URL: [${fullURL}]`)
-    return this.UrlModel.create({ fullURL, hash, shortURL })
+    // this.log.debug(`Criando a URL: [${fullURL}]`)
+    return this.urlModel.create({ fullURL, hash, shortURL })
   }
 
   async update(hash, fullURL) {
-    this.log.debug(`Atualizando a URL: [${fullURL}]`)
-    return this.UrlModel.findOneAndUpdate({ hash, fullURL })
+    // this.log.debug(`Atualizando a URL: [${fullURL}]`)
+
+    return this.urlModel.findOneAndUpdate(
+      { hash },
+      { fullURL },
+      { returnOriginal: false }
+    )
   }
 
-  async delete(hash, fullURL) {
-    this.log.debug(`Removendo a URL: [${fullURL}]`)
-    return this.UrlModel.findOneAndRemove({ hash, fullURL })
+  async delete(hash) {
+    // this.log.debug(`Removendo a URL: [${fullURL}]`)
+    return this.urlModel.findOneAndRemove({ hash })
   }
 }
 
