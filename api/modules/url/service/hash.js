9dbb55e6de59039c855684a894dbdb07aa796982
renanaribeiro91 <renan.admribeiro@gmail.com>
2023-03-02 17:21:20 -0300

    feat: hash service

---
+++ api/modules/url/service/hash.js
@@ -0,0 +1,21 @@
+const { BaseService } = require('simple-node-framework').Base;
+const { nanoid } = require('nanoid');
+const UrlRepository = require('../repository/url-reposittory.js');
+
+
+class HashService extends BaseService {
+    constructor() {
+        super({ module: HashService.name });
+        this.urlRepository = new UrlRepository()
+    }
+
+    async generateHash() {
+        return nanoid(5);       
+    }
+
+    async getUrlByHash(hash) {
+        return this.urlRepository.getHash(hash)
+    }    
+}
+
+module.exports = HashService;
