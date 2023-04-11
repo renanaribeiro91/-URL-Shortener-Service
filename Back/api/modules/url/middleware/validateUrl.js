c158c5f2065332bd1ad5c0a37fc092e8500eba51
renanaribeiro91 <renan.admribeiro@gmail.com>
2023-04-11 13:42:22 -0300

    fix

--- Back/api/modules/url/middleware/validateUrl.js
+++ Back/api/modules/url/middleware/validateUrl.js
@@ -1,18 +1,18 @@
-// const util = require('util')
-// const urlExists = util.promisify(require('url-exists'))
-// const { NOT_EXIST } = require('../../../shared/enum/ErrorEnum')
+const util = require('util')
+const urlExists = util.promisify(require('url-exists'))
+const { NOT_EXIST } = require('../../../shared/enum/ErrorEnum')
 
-// module.exports = async (req, res, next) => {
-//   try {
-//     const { fullURL } = req.body
+module.exports = async (req, res, next) => {
+  try {
+    const { fullURL } = req.body
 
-//     const isExist = await urlExists(fullURL)
+    const isExist = await urlExists(fullURL)
 
-//     if (!isExist) {
-//       return res.send(400, NOT_EXIST)
-//     }
-//     return next()
-//   } catch (error) {
-//     return next(error)
-//   }
-// }
+    if (!isExist) {
+      return res.send(400, NOT_EXIST)
+    }
+    return next()
+  } catch (error) {
+    return next(error)
+  }
+}
